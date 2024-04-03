28497a148bc5900409ac7a7cfac1c51264b768ee
Cy12334 <165625761+Cy12334@users.noreply.github.com>
2024-04-03 20:32:34 +0800

    Add files via upload

---
+++ Time_Converter/Time_Converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Converter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Time Converter!");
+        Console.WriteLine("Enter a value to convert:");
+
+        double value = Convert.ToDouble(Console.ReadLine());
+
+        ConvertSecondsToMinutes(value);
+        ConvertMinutesToHours(value);
+        ConvertHoursToDays(value);
+        ConvertDaysToMonths(value);
+
+        Console.WriteLine("Convert time!");
+    }
+
+    static void ConvertSecondsToMinutes(double seconds)
+    {
+        double minutes = seconds / 60;
+        Console.WriteLine(seconds + " seconds is equal to " + minutes + " minutes.");
+    }
+
+    static void ConvertMinutesToHours(double minutes)
+    {
+        double hours = minutes / 60;
+        Console.WriteLine(minutes + " minutes is equal to " + hours + " hours.");
+    }
+
+    static void ConvertHoursToDays(double hours)
+    {
+        double days = hours / 24;
+        Console.WriteLine(hours + " hours is equal to " + days + " days.");
+    }
+
+    static void ConvertDaysToMonths(double days)
+    {
+        double months = days / 30.42; // Approximate number of days in a month
+        Console.WriteLine(days + " days is equal to approximately " + months + " months.");
+    }
+}
